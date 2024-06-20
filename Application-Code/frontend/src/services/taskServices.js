965f2df23ef168e2533ed6b2a627628b4df8d72b
shubham9511s <shubham2025@gmail.com>
2024-06-20 12:16:15 +0530

    alb added

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = "http://localhost:5000/api/tasks" //"http://localhost:8080/api/tasks";
+const apiUrl = "http://k8s-threetie-mainlb-1dd958d0ec-352335056.eu-north-1.elb.amazonaws.com/api/tasks" //"http://localhost:8080/api/tasks";
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
